84535eebddd783c1f26cdb2bbeb8aeeb64b9147b
Md-Saif-Ali <119032699+Md-Saif-Ali@users.noreply.github.com>
2023-03-05 23:07:43 +0530

    Update music-list.js

--- music-list.js
+++ music-list.js
@@ -1,1 +1,54 @@
+const allMusics=[
+    {
+        songName:"Pasoori",
+        artistName: "Artist-1",
+        image: "images/music-1",
+        song: "songs/music-1.mp3",
+        songDuration: "3:44"
 
+    },
+
+    {
+        songName:"Besharam Rang",
+        artistName: "Artist-2",
+        image: "images/music-2",
+        song: "songs/music-2.mp3",
+        songDuration: "4:18"
+
+    },
+
+    {
+        songName:"Arabic kuthu",
+        artistName: "Artist-3",
+        image: "images/music-3",
+        song: "songs/music-3.mp3",
+        songDuration: "4:42"
+
+    },
+
+    {
+        songName:"Dippam Dappam",
+        artistName: "Artist-4",
+        image: "images/music-4",
+        song: "songs/music-4.mp3",
+        songDuration: "3:29"
+
+    },
+
+    {
+        songName:"ishare tere karti nigah",
+        artistName: "Artist-5",
+        image: "images/music-5",
+        song: "songs/music-5.mp3",
+        songDuration: "3:44"
+
+    },
+
+    {
+        songName:"Arcade",
+        artistName: "Artist-6",
+        image: "images/music-6",
+        song: "songs/music-6.mp3",
+        songDuration: "3:07"
+    },
+]
